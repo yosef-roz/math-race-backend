40c78dec437114721d15e0804554aba4eacfc0f4
Jake Walker <walker.jake.1299@gmail.com>
2026-03-02 11:28:04 +0200

    db config

--- src/main/java/com/example/math_race/utils/DatabaseConfig.java
+++ src/main/java/com/example/math_race/utils/DatabaseConfig.java
@@ -1,4 +1,72 @@
 package com.example.math_race.utils;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
 public class DatabaseConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() throws Exception {
+        String dbUser = env.getProperty("DB_ROOT");
+        String dbSchema = env.getProperty("DB_SCHEMA");
+        String dbPass = env.getProperty("DB_PASSWORD");
+        String host = env.getProperty("DB_HOST");
+        Integer port = env.getProperty("DB_PORT", Integer.class);
+
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+
+        String jdbcUrl = String.format("jdbc:mysql://%s:%d/%s?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
+                host, port, dbSchema);
+        dataSource.setJdbcUrl(jdbcUrl);
+
+        dataSource.setUser(dbUser);
+        dataSource.setPassword(dbPass);
+
+        dataSource.setMaxPoolSize(20);
+        dataSource.setMinPoolSize(5);
+        dataSource.setInitialPoolSize(5);
+        dataSource.setIdleConnectionTestPeriod(3600);
+        dataSource.setTestConnectionOnCheckin(true);
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() throws Exception {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+        hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
+        hibernateProperties.put("hibernate.jdbc.batch_size", 50);
+        hibernateProperties.put("hibernate.connection.characterEncoding", "utf8");
+        hibernateProperties.put("hibernate.enable_lazy_load_no_trans", "true");
+
+        sessionFactoryBean.setHibernateProperties(hibernateProperties);
+
+        sessionFactoryBean.setMappingResources("objects.hbm.xml");
+
+        return sessionFactoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() throws Exception {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
 }
