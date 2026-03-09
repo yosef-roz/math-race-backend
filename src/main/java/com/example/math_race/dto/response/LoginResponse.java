92efcf515a1cd53db28db80e473f3181bd81472c
Shimon <shimon@>
2026-03-09 12:37:30 +0200

    Changes by rozen

--- src/main/java/com/example/math_race/dto/response/LoginResponse.java
+++ src/main/java/com/example/math_race/dto/response/LoginResponse.java
@@ -1,4 +1,12 @@
 package com.example.math_race.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class LoginResponse {
+    private String token;
+    private String username;
+    private String email;
 }
--- src/main/java/com/example/math_race/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/math_race/exception/GlobalExceptionHandler.java
@@ -1,8 +1,25 @@
 package com.example.math_race.exception;
 
+import com.example.math_race.dto.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(LogicException.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(LogicException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
+
+        ApiResponse<Void> body = ApiResponse.error(
+                errorCode.getCode(),
+                errorCode.getMessage()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(body);
+    }
 }
