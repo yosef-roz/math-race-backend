83c71798fe61d0e4adf4d7c7a8a0f1646d9ac6c7
Shimon <shimon@>
2026-05-20 09:13:28 +0300

    update

--- src/main/java/com/example/math_race/service/EmailService.java
+++ src/main/java/com/example/math_race/service/EmailService.java
@@ -7,7 +7,6 @@ import com.example.math_race.entities.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
